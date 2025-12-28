621494e7e9537b2bfa741ce7ee59bb49a8d13157
Leandro <leandrogpelozo1@gmail.com>
2025-12-28 09:16:54 -0300

    Tomamos un dato mas del mercado (ATR)

--- data/market_data.py
+++ data/market_data.py
@@ -1,14 +1,25 @@
 import yfinance as yf
 import pandas as pd
 
-def get_historical_data(symbol, start_date, end_date):
+def get_historical_data(ticker, start_date, end_date):
     """
     Descarga datos historicos diarios de una accion
     """
-    data=yf.download(
-        symbol,
-        start=start_date,
-        end=end_date,
-        interval='1d'
-    )
+    data = yf.download(ticker, start=start_date, end=end_date, interval='1d')
+    # hig - low
+    high_low = data['High'] - data['Low']
+
+    #high - close anterior
+    high_close = (data['High'] - data['Close'].shift()).abs()
+
+    #low - close anterior
+    low_close = (data['Low'] - data['Close'].shift()).abs()
+
+    # True range (El mayor de los 3)
+    true_range = pd.concat([high_low, high_close, low_close], axis=1).max(axis=1)
+
+    data['ATR'] = true_range.rolling(14).mean()
+
+    print(data[["Close", "ATR"]].tail())
+
     return data
